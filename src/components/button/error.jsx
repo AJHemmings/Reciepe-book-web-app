434476929ed83212d3ed21f92b4c9da20c5704bd
Adam Hemmings <38017169+Makenshii@users.noreply.github.com>
2025-02-11 13:24:15 +0000

    fixed error button

---
+++ src/components/button/error.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function ErrorButton() {
+  const handleClick = () => {
+    try {
+      throw new Error("This is your first error!");
+    } catch (error) {
+      console.error("Caught an error:", error.message);
+      // You can also display the error to the user or handle it in other ways
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>Break the world</button>
+    </div>
+  );
+}
+
+export default ErrorButton;
