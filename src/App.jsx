963d823fe55d1e40ed18f85849f898c9a3dfff5f
Adam <38017169+AJHemmings@users.noreply.github.com>
2025-03-24 14:55:03 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Footer from "./components/footer/footer";
 import Form from "./components/form/form";
 import FlashcardsContainer from "./components/flashcards/flashcards";
 import "./components/header/header.css";
-import ErrorButton from "./components/button/error";
 
 const recipeReducer = (state, action) => {
   switch (action.type) {
